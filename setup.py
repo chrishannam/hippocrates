6cb2ec4a96888d22f2ccc3023cc129366a2f8a35
Chris Hannam <ch@chrishannam.co.uk>
2019-02-27 21:09:57 +0000

    Add options, update deps

--- setup.py
+++ setup.py
@@ -1,24 +1,27 @@
 import setuptools
 
+import hippocrates
+
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='hippocrates',
-    version='0.1.4',
+    version=hippocrates.__version__,
     author='Chris Hannam',
     author_email='ch@chrishannam.co.uk',
     description='Mental health questionnaires.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/chrishannam/hippocrates',
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=('tests',)),
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
     ],
     install_requires=[
+        'click==7.0',
         'pick>=0.6.4',
         'tabulate>=0.8.2',
     ],
