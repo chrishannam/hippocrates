eae45a3d4e285de6da4096148d5600ff903db6a5
chrishannam <ch@chrishannam.co.uk>
2020-02-19 12:54:55 +0000

    Adds example results.csv and script to generate.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,13 +5,19 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
-* Latest requirements.
+### Added
+- Add script to generate test data for reporting tests and graphing. 
+
+### Changed
+- Update requirements to latest.
+- Fix PHQ2 score results
+- Fix GAD2 score results
 
 ## 0.3.2
 ### Added
-* New executables to take questionnaires.
-* Logging option.
-* Hide results option.
+- New executables to take questionnaires.
+- Logging option.
+- Hide results option.
 
 ## 0.1.4
-Base release
+- Base release
---
+++ hippocrates/utils/generator_results.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""
+Create a results.csv file for GAD7 and PHQ9 tests for the previous year.
+"""
+
+import csv
+from datetime import datetime, timedelta
+from random import randint
+
+from hippocrates.questionnaires.base import DATE_FORMAT
+
+
+def main():
+    with open('results.csv', 'w', newline='') as csvfile:
+        fieldnames = ['Date Taken', 'Assessment Name', 'Score']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for day in range(1, 365):
+            phq9_date = datetime.now() - timedelta(days=day)
+            gad7_date = phq9_date - timedelta(seconds=300)
+
+            phq9_date = phq9_date.strftime(DATE_FORMAT)
+            gad7_date = gad7_date.strftime(DATE_FORMAT)
+            writer.writerow(
+                {
+                    'Date Taken': phq9_date,
+                    'Assessment Name': 'phq9',
+                    'Score': randint(0, 27)
+                }
+            )
+            writer.writerow(
+                {
+                    'Date Taken': gad7_date,
+                    'Assessment Name': 'phq9',
+                    'Score': randint(0, 21)
+                }
+            )
+
+
+if __name__ == '__main__':
+    main()
