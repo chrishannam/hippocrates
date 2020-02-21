36d665755c7fe44f600c217f9c723c1d4200879c
chrishannam <ch@chrishannam.co.uk>
2020-02-21 13:55:05 +0000

    Add new questionnaire.

---
+++ hippocrates/questionnaires/mood_tracker/assessment.py
@@ -0,0 +1,32 @@
+"""
+Quick and simple mood logger.
+"""
+
+import typing as t
+from pathlib import Path
+
+from hippocrates.questionnaires.base import Assessment
+from hippocrates.questionnaires.models import (
+    create_questions,
+    create_results,
+    import_question_set,
+)
+
+JSON_QUESTION_SET = version_path = Path(__file__).parent / 'question_set.json'
+
+question_set_json = import_question_set(path=JSON_QUESTION_SET)
+question_set = create_questions(question_set_json)
+
+
+class CustomMoodAssessment(Assessment):
+    name: str = 'mood'
+
+    def __init__(self):
+        raw_json = import_question_set(path=JSON_QUESTION_SET)
+        self.question_set: t.List = create_questions(raw_json)
+        self.total_questions: int = len(self.question_set)
+        self.results: t.List = create_results(question_set_json=raw_json)
+
+    @classmethod
+    def length(cls):
+        return len(question_set)
