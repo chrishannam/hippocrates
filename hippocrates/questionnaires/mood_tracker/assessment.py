3956a9d763b227ec0da4cd0f40fe153084cc06bf
chrishannam <ch@chrishannam.co.uk>
2020-02-21 19:32:09 +0000

    Fix result.

--- hippocrates/questionnaires/mood_tracker/assessment.py
+++ hippocrates/questionnaires/mood_tracker/assessment.py
@@ -10,7 +10,7 @@ from hippocrates.questionnaires.models import (
     create_questions,
     create_results,
     import_question_set,
-)
+    Result)
 
 JSON_QUESTION_SET = version_path = Path(__file__).parent / 'question_set.json'
 
@@ -23,7 +23,7 @@ class CustomMoodAssessment(Assessment):
 
     def __init__(self):
         raw_json = import_question_set(path=JSON_QUESTION_SET)
-        self.question_set: t.List = create_questions(raw_json)
+        self.question_set: t.List = question_set
         self.total_questions: int = len(self.question_set)
         self.results: t.List = create_results(question_set_json=raw_json)
 
